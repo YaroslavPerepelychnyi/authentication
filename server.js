9da9141f684b258d39913f9d38f00d95826d196c
Yaroslav Perepelychnyi <Yaroslav_Perepelychnyi@epam.com>
2020-06-18 14:28:55 +0300

    add passport for auth

--- server.js
+++ server.js
@@ -1,44 +1,88 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
 
-app.use(express.json());
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id),
+);
 
 const users = [];
 
-app.get('/users', (req, res) => {
-    res.json(users);
-})
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.post('/users', async (req, res) => {
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {
+        name: req.user.name
+    }); 
+});
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs'); 
+});
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs'); 
+});
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = {
+        users.push({
+            id: Date.now().toString(),
             name: req.body.name,
+            email: req.body.email,
             password: hashedPassword
-        }
-        users.push(user);
-        res.status(201).send();
+        });
+        res.redirect('/login');
     } catch {
-        res.status(500).send();
+        res.redirect('/register');
     }
+});
+
+app.post('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/');
 })
 
-app.post('/users/login', async (req, res) => {
-    const user = users.find(user => req.body.name == user.name);
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
 
-    if (user == null) {
-        return res.status(404).send('Cannot find user');
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
     }
-    try {
-        if ( await bcrypt.compare(req.body.password, user.password)) {
-            res.send('Success');
-        } else {
-            res.send('Not Allowed');
-        }
-    } catch {
-        res.status(500).send('Error');
-    }
-})
+    next();
+}
+
 
 app.listen(3001);
